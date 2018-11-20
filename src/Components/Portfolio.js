46be456f1fd8e3217a01416cf12c73d80dbb7dff
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-11-20 11:55:56 -0500

    background image

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import styled from "styled-components";
+
+const StyledBackground = styled.div`
+  background-color: black;
+  background-image: url("https://i.imgur.com/HznhsUp.jpg");
+  background-size: cover;
+  /* background-attachment: fixed; */
+  background-position: center;
+  /* background-repeat: no-repeat; */
+  /* background-size: cover; */
+  height: 100vh;
+  opacity: .5;
+  a {
+    text-decoration: none;
+  }
+`;
+
+
 
 export default class Portfolio extends Component {
   render() {
     return (
       <div>
-        1 photo. 3 cards
+        <StyledBackground>
+
+           
+           
+        </StyledBackground> 
       </div>
     )
   }
